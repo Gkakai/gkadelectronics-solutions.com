48483d69d7aa8ccc1350a2e2c9d9204b02ad894c
Gilbert kakai <kakaigilbert@yahoo.com>
2023-04-06 21:05:26 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+        <div class="container">
+
+            <div class="section-title">
+                <center> <h1>ABOUT</h1></center>
+
+                <p>Gkad ia a company that offers you affodable services whenever you need it..</p>
+            </div>
+
+            <div class="row">
+                <marquee><div class="col-lg-6">
+                    <img src="images/assembling-and-service.jpg" class="img-fluid" alt="">
+                </div></marquee>
+                <div class="col-lg-6 pt-4 pt-lg-0 content">
+                    <h3>The best services.</h3>
+                    <p class="fst-italic">
+                        That will Give you a long term solution
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check-circle"></i> High Quality Products.</li>
+                        <li><i class="bi bi-check-circle"></i> Less cost.</li>
+                        <li><i class="bi bi-check-circle"></i> Free delivary on all products.</li>
+                    </ul>
+
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Section -->
---
+++ team.php
@@ -0,0 +1,73 @@
+<?phph
+include "header.php";
+?>
+
+ <section id="team" class="team">
+        <div class="container">
+
+            <div class="section-title">
+                <center class="bg-primary ">
+                    <h2>Team</h2>
+                </center>
+
+                <p>This is our able team that is resposnsiple for your work</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="images/teams/Founders.jpg" class="img-fluid" alt="">
+
+                        </div>
+                        <div class="member-info">
+                            <h4>Gilbert & Kagwe & Dickson</h4>
+                            <span>Founders</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="images/teams/Dicson.jpg" class="img-fluid" alt="">
+
+                        </div>
+                        <div class="member-info">
+                            <h4>Dickson Kibet</h4>
+                            <span>Admin</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                       <div class="member-img">
+                            <img src="images/teams/gilbert.jpg" class="img-fluid" alt="">
+
+                        </div>
+                        <div class="member-info">
+                            <h4>Olekaka Gilbert</h4>
+                            <span>Hardware Technician</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <div class="member-img">
+                            <img src="images/teams/peter.jpg" class="img-fluid" alt="">
+
+                        </div>
+                        <div class="member-info">
+                            <h4>Peter Kagwe</h4>
+                            <span>Software Technician</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Team Section -->
